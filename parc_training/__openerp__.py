4dcccbe6353e1abc39d1f1069942faaca4aed3d7
Meftah <mmeftah@kazacube.com>
2023-01-24 12:01:08 +0100

    [IMP] creation de modele

---
+++ parc_training/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Formation_gestion_de_parc",
+
+    'summary': """
+        Module dévelopé au cours de la formation technique""",
+
+    'description': """
+       Module dévelopé au cours de la formation technique
+    """,
+
+    'author': "Meftah Mohamed",
+    'website': "http://www.kazacube.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Formation',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+
+    ],
+}
